3d7c14e95c48efc05adccfb1036be096ac7dd242
Youness KAFIA <kafia.youness@hotmail.fr>
2021-04-19 17:12:51 +0200

    Created volume

---
+++ RayMarching/CustomRenderers/CustomComponents/GridTextureComponent.cs
@@ -0,0 +1,28 @@
+﻿using Stride.Core;
+using Stride.Core.Mathematics;
+using Stride.Engine;
+using Stride.Engine.Design;
+using Stride.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayMarching
+{
+    [DataContract("GridTextureCompnent")]
+    [Display("GridTextureCompnent", Expand = ExpandRule.Once)]
+    [DefaultEntityComponentRenderer(typeof(GridTextureEntityProcessor))]
+    [ComponentOrder(100)]
+    public class GridTextureComponent : ActivableEntityComponent
+    {
+        [DataMember(10)]
+        [Display("3D Grid")]
+        public Texture GridTexture { get; set; }
+
+        [DataMember(20)]
+        [Display("Grid Dimensions")]
+        public Int3 Dimensions { get; set; }
+    }
+}
---
+++ RayMarching/CustomRenderers/CustomComponents/GridTextureEntityProcessor.cs
@@ -0,0 +1,76 @@
+﻿using Stride.Engine;
+using Stride.Graphics;
+using Stride.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayMarching
+{
+    public class GridTextureEntityProcessor : EntityProcessor<GridTextureComponent, GridTextureObject>, IEntityComponentRenderProcessor
+    {
+        public VisibilityGroup VisibilityGroup { get; set; }
+
+        /// <summary>
+        /// Gets the active render object.
+        /// </summary>
+        /// <value>The active render object.</value>
+        public GridTextureObject MyRenderObject { get; private set; }
+
+        /// <inheritdoc />
+        protected override void OnSystemRemove()
+        {
+            if (MyRenderObject != null)
+            {
+                VisibilityGroup.RenderObjects.Remove(MyRenderObject);
+                MyRenderObject = null;
+            }
+
+            base.OnSystemRemove();
+        }
+
+        /// <inheritdoc />
+        protected override GridTextureObject GenerateComponentData(Entity entity, GridTextureComponent component)
+        {
+            return new GridTextureObject() 
+            {
+                GridTexture = component.GridTexture,
+                Dimensions = component.Dimensions
+            };
+        }
+
+        
+
+        /// <inheritdoc />
+        public override void Draw(RenderContext context)
+        {
+            var previousRenderObject = MyRenderObject;
+            MyRenderObject = null;
+
+            // Go thru components of this entity
+            foreach (var entityKeyPair in ComponentDatas)
+            {
+                var myEntityComponent = entityKeyPair.Key;
+                var myRenderObject = entityKeyPair.Value;
+                if (myEntityComponent.Enabled)
+                {
+                    // Select the first enabled component and assign data from UI
+
+                    myEntityComponent.GridTexture = myRenderObject.GridTexture;
+                    MyRenderObject = myRenderObject;
+                    break;
+                }
+            }
+
+            if (MyRenderObject != previousRenderObject)
+            {
+                if (previousRenderObject != null)
+                    VisibilityGroup.RenderObjects.Remove(previousRenderObject);
+                if (MyRenderObject != null)
+                    VisibilityGroup.RenderObjects.Add(MyRenderObject);
+            }
+        }
+    }
+}
